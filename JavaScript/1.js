5ba6c7349bf660dfcbc05724f9832b3ce32191b5
Braian Henrique Moreira <braianhenriquemoreira@Gmail.com>
2026-09-23 09:01:13 -0300

    JS, Lista 1
    pasta da lista 1
    inicio JavaScript

---
+++ JavaScript/1.js
@@ -0,0 +1,9 @@
+console.log("RECEBA!");
+// Declarar variaveis em JS.
+i=0; //variavel global var
+var nome="umelhordomundo"; //define variavel global
+nome=1;
+let numero; //define variavel local por bloco
+const numeroPi = 3.1415;
+console.log("Variavel i:" +typeof(i));
+console.log("Variavel nome:" +typeof(nome));
